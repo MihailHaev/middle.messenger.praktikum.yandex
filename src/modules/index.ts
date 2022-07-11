4d3a30955596cbc4c8eff03a9b5ca9dbe27f2866
ma-havaev <ma-havaev@yandex-team.ru>
2022-07-11 03:15:31 +0300

    fix router for checks

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,7 @@
+export * from './Router';
 export * from './Block';
 export * from './EventBus';
 export * from './HTTPTransport';
 export * from './registerComponent';
 export * from './renderDOM';
-export { Router } from './Router';
 export * from './Store';
